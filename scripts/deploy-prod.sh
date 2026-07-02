2767de3dbf597e651755879f314288a4a8e71bac
Naeem <naeem.dstech@gmail.com>
2026-07-02 16:50:50 +0500

    deployment test

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -3,6 +3,9 @@ set -e
 
 COMPOSE_FILE="${COMPOSE_FILE:-docker-compose.prod.yml}"
 
+docker container prune -f
+docker image prune -af
+docker builder prune -af
 docker compose -f "$COMPOSE_FILE" pull backend
 docker compose -f "$COMPOSE_FILE" up -d
 docker compose -f "$COMPOSE_FILE" exec -T backend npm run migration:run
